5284932e1200a9ca6aef25edc3a5635145b5b437
Dominik Skála <DominikSkala@seznam.cz>
2021-10-23 21:00:17 +0200

    prep flask

--- src/main.py
+++ src/main.py
@@ -3,19 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import Config
-import Migrator
+
+from app import app
 
 
-def main():
-    c = Config.Config('http://sc02.fi.muni.cz', 'a1875f9c-8c91-41a3-a720-dc5caee3b4d1', '00281344')
-    m = Migrator.Migrator(c)
-    m.migrate()
+#def main():
+#    c = Config.Config('http://sc02.fi.muni.cz', 'a1875f9c-8c91-41a3-a720-dc5caee3b4d1', '00281344')
+#    m = Migrator.Migrator(c)
+#    m.migrate()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    app.run()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
